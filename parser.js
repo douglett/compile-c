2339ad37f48f35ef580b414219e7b1e17f050d94
doug@luna <doug@totem9.net>
2019-03-31 17:32:48 +0100

    testing outputting to lisp format

--- parser.js
+++ parser.js
@@ -21,107 +21,21 @@ const Parser = new function() {
 	
 	// main parse function
 	const parseprog = () => {
-		const prog = this.prog = { type:'prog', defines: [], functions:[] };
+		// const prog = this.prog = { type:'prog', defines: [], functions:[] };
+		const prog = this.prog = [];
 		let t;
 		while (!error) {
-			if (t = p_declare_line()) prog.defines.push(t);
-			else if (t = p_function()) prog.functions.push(t);
+			if      (t = p_declare_line()) prog.push(t); // prog.defines.push(t);
+			else if (t = p_function()) prog.push(t); // prog.functions.push(t);
 			else break;
 		}
+		console.log('prog:', prog);
+
 		if (!error && !accept('EOF'))
 			error = `expected end of program (EOF): ${tok.peek().str()}`;
-		precompile(this.prog);
 		if (error) 
 			return console.error(error), false;
 		return true;
-	};
-
-
-
-	// precompile - do basic sanity checker, so we don't have to do it in the compiler
-	const precompile = () => {
-		const def = [];
-		let fndef = [];
-		let context = 'global';
-		// 
-		const exists     = (name) => !!def.find(d => d.name === name);
-		const exists_ctx = (name) => !!def.find(d => d.name === name && d.ctx === context);
-		//
-		const check = (ast) => {
-			let pos = def.length;
-			let fn;
-			switch (ast.type) {
-				// main program
-				case 'prog':
-					def.splice(0);
-					context = 'global';
-					// global defines
-					ast.defines.some(t => check(t), error);
-					if (error) break;
-					// check function contents
-					context = 'local';
-					fndef = ast.functions;
-					ast.functions.some(t => check(t), error);
-					break;
-				// check defines
-				case 'define':
-					if (exists_ctx(ast.name)) 
-						return error = `variable already defined: ${ast.name}`, 1;
-					def.push({ name: ast.name, ctx: context });
-					if (ast.val) check(ast.val);
-					break;
-				// check functions
-				case 'function':
-					ast.arguments.some(t => check(t), error);
-					ast.lines.some(t => check(t), error);
-					def.splice(0, pos);
-					break;
-				// various functionality
-				case 'assign':
-					if (!exists(ast.name))
-						return error = `missing variable in assignment: ${ast.name}`, 1;
-					check(ast.val);
-					break;
-				case 'word': // identifier in expression
-					if (!exists(ast.token))
-						return error = `missing variable in expression: ${ast.token}`, 1;
-					break;
-				case 'call':
-					// check function definitions
-					// console.log('call', ast, fndef);
-					if (ast.name === 'puti')
-						return console.warn('TODO: puti checking'), 0;
-					if (!(fn = fndef.find(f => f.name === ast.name)))
-						return error = `call to undefined function: ${ast.name}`, 1;
-					if (ast.arguments.length !== fn.arguments.length)
-						return error = `wrong number of arguments to function: ${ast.name}`, 1;
-					break;
-				case '*':
-				case '+':
-				case '==':
-				case '>':
-					check(ast.vala);
-					check(ast.valb);
-					break;
-				case 'if':
-				case 'while':
-					check(ast.val);
-					break;
-				case 'expression':
-				case 'return':
-					if (ast.val) check(ast.val);
-					break;
-				// OK - skip these
-				case 'number':
-					break;
-				// unknown - error
-				default:
-					// console.log('sanity', ast.type);
-					return error = `precompile: unknown input: ${ast.type}`, 1;
-			}
-		};
-		// do check
-		return check(this.prog);
 	};
 
 
@@ -151,7 +65,8 @@ const Parser = new function() {
 		t = accept('operator', '=');
 		ex = p_expr();
 		if (t && ex)
-			return def.val = ex, def;
+			// return def.val = ex, def;
+			return def.push(ex), def;
 		return tok.pos = p, def;  // naked declaration
 	};
 	// declaration specifier (type name)
@@ -159,8 +74,10 @@ const Parser = new function() {
 		let p = tok.pos, type, name;
 		type = p_type();
 		name = p_ident();
-		if (type && name) 
-			return { type:'define', name:name.token, deftype:type.token };
+		if (type && name) {
+			// return { type:'define', name:name.token, deftype:type.token };
+			return [ 'define', `$${name.token}` ];
+		}
 		return tok.pos = p, null;
 	};
 
@@ -168,15 +85,16 @@ const Parser = new function() {
 
 	// functions
 	const p_function = () => {
-		let p = tok.pos, t, def;
+		let p = tok.pos, t, def, lines = [];
 		if (!(def = p_function_def())) return null;
-		def.type = 'function', def.lines = [];
+		// def.type = 'function', def.lines = [];
+		def.push(lines);
 		while ((t = p_statement()) && !error)
-			def.lines.push(t);
+			// def.lines.push(t);
+			lines.push(t);
 		if (accept('operator', '}')) return def;
 		if (!error)
 			error = `unexpected token in function: ${tok.peek().str()}`;
-		// return null;
 		return def;
 	};
 	// function definition
@@ -185,9 +103,11 @@ const Parser = new function() {
 		type = p_type();
 		ident = p_ident();
 		if (type && ident) {
-			def = { type:'function_def', name:ident.token, deftype:type.token };
+			// def = { type:'function_def', name:ident.token, deftype:type.token };
+			def = [ 'defun', `$${ident.token}` ]
 			t = accept('operator', '(') ;
-			def.arguments = p_function_args();
+			// def.arguments = p_function_args();
+			def.push(p_function_args());
 			t = t && accept('operator', ')') && accept('operator', '{');
 			if (type && ident && t) 
 				return def;
@@ -206,15 +126,17 @@ const Parser = new function() {
 	};
 	// function call
 	const p_call = () => {
-		let p = tok.pos, t, id, def;
+		let p = tok.pos, t, id, args /*, def*/;
 		id = p_ident();
 		t = accept('operator', '(');
 		if (id && t) {
-			def = { type:'call', name:id.token };
-			def.arguments = p_call_args();
+			// def = { type:'call', name:id.token };
+			// def.arguments = p_call_args();
+			args = p_call_args();
 			t = t && accept('operator', ')');
 			if (id && t)
-				return def;
+				// return def;
+				return [ 'call', `@${id.token}`, args ];
 		}
 		return tok.pos = p, null;
 	};
@@ -249,7 +171,8 @@ const Parser = new function() {
 		b = p_expr();
 		t = t && accept('operator', ';');
 		if (a && t && b)
-			return { type:'assign', name:a.token, val:b };
+			// return { type:'assign', name:a.token, val:b };
+			return [ 'set', `$${a.token}`, b ];
 		return tok.pos = p, null;
 	};
 	// line expression
@@ -258,7 +181,8 @@ const Parser = new function() {
 		e = p_expr();
 		t = accept('operator', ';');
 		if (e && t) 
-			return { type:'expression', val:e };
+			// return { type:'expression', val:e };
+			return e;
 		return tok.pos = p, null;
 	};
 	// keyword statement
@@ -269,7 +193,8 @@ const Parser = new function() {
 			e = p_expr();
 			t = accept('operator', ';');
 			if (s && e && t)
-				return { type:'return', val:e };
+				// return { type:'return', val:e };
+				return [ 'return', e ];
 		}
 		// if statement
 		else if (s = accept('word', 'if')) {
@@ -278,7 +203,8 @@ const Parser = new function() {
 			t = t && accept('operator', ')');
 			if (s && t && e) 
 				if (def = p_block())
-					return def.type = 'if', def.val = e, def;
+					// return def.type = 'if', def.val = e, def;
+					return [ 'if', e, def ];
 		}
 		// while statement
 		else if (s = accept('word', 'while')) {
@@ -287,7 +213,8 @@ const Parser = new function() {
 			t = t && accept('operator', ')');
 			if (s && t && e) 
 				if (def = p_block())
-					return def.type = 'while', def.val = e, def;
+					// return def.type = 'while', def.val = e, def;
+					return [ 'while', e, def ];
 		}
 		// unknown
 		else
@@ -300,12 +227,14 @@ const Parser = new function() {
 	// block of statements
 	const p_block = () => {
 		let p = tok.pos, t, a;
-		let def = { type:'block', lines:[] };
+		// let def = { type:'block', lines:[] };
+		let def = [];
 		t = accept('operator', '{');
 		if (!t && !error)
 			error = `expected open braces in block: ${tok.peek().str()}`;
 		while ((a = p_statement()) && !error)
-			def.lines.push(a);
+			// def.lines.push(a);
+			def.push(a);
 		t = t && accept('operator', '}');
 		if (!t && !error)
 			error = `expected close braces in block: ${tok.peek().str()}`;
@@ -327,7 +256,8 @@ const Parser = new function() {
 		c = (q ? q.token : '') + ( o ? o.token : '');
 		if (a && b && c)
 			if (/^(==|<|<=|>|>=)$/.test(c))
-				return { type:c, vala:a, valb:b };
+				// return { type:c, vala:a, valb:b };
+				return [ c, a, b ];
 		return tok.pos = p, null;
 	};
 	// additive expressions
@@ -338,7 +268,8 @@ const Parser = new function() {
 		if (a && !o) return a;
 		b = p_expr_mul();
 		if (a && o && b)
-			return { type:o.token, vala:a, valb:b };
+			// return { type:o.token, vala:a, valb:b };
+			return [ o.token, a, b ];
 		return tok.pos = p, null;
 	};
 	// mupliplicative expression
@@ -349,15 +280,16 @@ const Parser = new function() {
 		if (a && !o) return a;
 		b = p_expr_primary();
 		if (a && o && b)
-			return { type:o.token, vala:a, valb:b };
+			// return { type:o.token, vala:a, valb:b };
+			return [ o.token, a, b ];
 		return tok.pos = p, null;
 	};
 	// primary expression
 	const p_expr_primary = () => {
 		let p = tok.pos, t;
-		if (t = accept('number')) return t;
+		if (t = accept('number')) return t.token;
 		if (t = p_call()) return t;
-		if (t = p_ident()) return t;
+		if (t = p_ident()) return `$${t.token}`;
 		if (accept('operator', '(')) {
 			t = p_expr();
 			if (accept('operator', ')')) return t;
@@ -383,6 +315,25 @@ const Parser = new function() {
 				});
 			else 
 				out = out.substr(0, out.length-1) + `${obj}\n`;
+		}
+		show(this.prog, 0);
+		return out;
+	};
+
+	this.showlist = () => {
+		let out = '';
+		function show(list, indent) {
+			out += `${'\t'.repeat(indent)}(`;
+			let lastarr = 0;
+			list.forEach((k, i) => {
+				if (Array.isArray(k)) 
+					out += `\n`, lastarr = 1, show(k, indent+1);
+				else if (lastarr)
+					out += `\n${'\t'.repeat(indent+1)}${k}`, lastarr = 0;
+				else
+					out += (i > 0 ? ' ' : '') + k;
+			});
+			out += ')';
 		}
 		show(this.prog, 0);
 		return out;
