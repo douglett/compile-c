c8ab05a7b2131b0dfb8e69d6096efd49f09992b7
Douglas Inman <Douglas.Inman@strategic-i.com>
2019-04-03 12:29:58 +0100

    new list validator

--- lisper.js
+++ lisper.js
@@ -1,113 +1,142 @@
 'use strict';
 
 const Lisper = new function() {
-	let error = '';
-	this.list = [];
-
-	this.parse = (str) => { };
-	this.parseTokens = (list) => { };
-	this.parseList = (list) => {
-		this.list = list;
-		validate(list);
+	let error = '', pos = 0;
+	let list = [];
+	
+	this.validateList = (ls) => {
+		list = [];
+		const flatten = (l) => {
+			if (l instanceof Array)
+				list.push('('),
+				l.forEach(l => flatten(l)),
+				list.push(')');
+			else
+				list.push(l);
+		};
+		ls.forEach(l => flatten(l));
+		validate();
+	};
+	this.validate = (ls) => {
+		list = ls;
+		validate();
 	};
 	this.error = () => error;
 
 
-
-	const is_vname  = (str) => /^\$[A-Za-z_][A-Za-z_0-9]*$/.test(str);
-	const is_fname  = (str) => /^\@[A-Za-z_][A-Za-z_0-9]*$/.test(str);
-	const is_num    = (str) => /^[0-9]+$/.test(str);
-	const v_vname   = (str) => is_vname(str) ? null : error = `expected variable name [${str}]`;
-	const v_fname   = (str) => is_fname(str) ? null : error = `expected function name [${str}]`;
-	const v_num     = (str) => is_num  (str) ? null : error = `expected number [${str}]`;
-
-
-	const validate = (list) => {
-		error = '';
-		// top level
-		if (!(list instanceof Array))
-			error = `expected list at top level`;
-		list.some(l => {
-			if (error) 
-				return error;
-			if (list instanceof Array && l[0] === 'define')
-				return v_define(l);
-			if (list instanceof Array && l[0] === 'defun')
-				return v_defun(l);
-			return error = `expected define of defun at main block level`;
-		});
-		// error checking
-		return error 
-			? (console.error(`Lisper: ${error}`), false)
-			: (console.log(`Lisper: validate OK`), true);
+	const validate = () => {
+		console.log(list);
+		error = '', pos = 0;
+		while (pos < list.length) 
+			if      (error) break;
+			else if (v_define()) ;
+			else if (v_defun()) ;
+			else if (!error) error = `script: expected define or defun`;
+		console.log(error, pos);
 	};
 
-	const v_define = (list) => {
-		const name = list[1];
-		const ex = list[2];
-		if (!is_vname(name))
-			return error = `define: expected variable name [${name}]`;
-		if (ex) v_expr(ex);
+	// short validators
+	const v_lstart  = () => list[pos] === '(' ? (++pos, true) : false;
+	const v_lend    = () => list[pos] === ')' ? (++pos, true) : false;
+	const v_varid   = () => /^\$[A-Za-z_][A-Za-z0-9_]*$/.test(list[pos]) ? (++pos, true) : false;
+	const v_funcid  = () => /^\@[A-Za-z_][A-Za-z0-9_]*$/.test(list[pos]) ? (++pos, true) : false;
+	const v_num     = () => /^[0-9]+$/.test(list[pos]) ? (++pos, true) : false;
+
+
+	const v_atom = () => {
+		typeof list[pos] === 'string' && list[pos] !== '(' && list[pos] !== ')'
+			? (++pos, true) : false;
+	};
+	const v_list = () => {
+		if (!v_lstart()) return false;
+		// debugger
+		while (pos < list.length)
+			if      (list[pos] === ')') break;
+			else if (v_list() || v_atom()) ;
+			else    return false;
+		if (!v_lend()) return false;
+		return true;
 	};
 
-	const v_defun = (list) => {
-		const name = list[1];
-		const args = list[2];
-		const block = list[3];
-		if (v_fname(name)) 
-			return `defun: ${error}`;
-		if (!(args instanceof Array))
-			return error = `defun: expected arguments list [${name}]`;
-		args.some(a => ( v_define(a), error ));
-		if (error) return error;
-		v_block(block);
+
+	const v_define = () => {
+		if (list[pos] !== '(' || list[pos+1] !== 'define') return false;
+		pos += 2;
+		if (!v_varid()) return error = `define: expected variable name`, false;
+		if (!v_expr() && error) return false;
+		if (!v_lend()) return error = `define: expected list end`, false;
+		return true;
 	};
 
-	const v_expr = (list) => {
-		if (list instanceof Array) {
-			if (/^(\+|\-|\*|\\|==|>=|<=|>|<)$/.test(list[0]))
-				for (let i = 1; i < list.length; i++)
-					v_expr(list[i]);
-			else if (list[0] === 'call') {
-				const name = list[1];
-				const args = list[2];
-				if (v_fname(name)) return error;
-				if (!(args instanceof Array))
-					return error = `expected arguments list in call [${name}]`;
-			}
+
+	const v_defun = () => {
+		if (list[pos] !== '(' || list[pos+1] !== 'defun') return false;
+		pos += 2;
+		if (!v_funcid()) return error = `define: expected function name`, false;
+		// argument list
+		if (!v_lstart()) return error = `define: expected argument list`, false;
+		while (pos < list.length)
+			if      (list[pos] === ')') break;
+			else if (v_define()) ;
+			else if (!error) return error = `define: unexpected argument [${list[pos]}`, false;
+		if (!v_lend()) return error = `define: expected list end`, false;
+		// function body
+		if (!v_block()) return (!error ? error = `define: expected function block` : null), false;
+		return true;
+	};
+
+	const v_expr = () => {
+		// console.log(pos, list[pos], v_num(list[pos]))
+		if (v_varid()) return true;
+		if (v_num()) return true;
+		if (v_lstart()) {
+			if (!/^(\+|\-|\*|\\)$/.test(list[pos])) 
+				return error = `expression: unknown command [${list[pos]}]`, false;
+			pos++;
+			while (pos < list.length)
+				if      (error) return false;
+				else if (list[pos] === ')') break;
+				else    e_expr();
+			if (!v_lend()) return error = `expression: expected list end`, false;
 		}
-		else if (is_vname(list)) ;
-		else if (is_num(list)) ;
-		else 
-			return error = `unknown token in expression [${list}]`;
+		return false; // not an expression
 	};
 
-	const v_block = (list) => {
-		let name, ex, block;
-		if (!(list instanceof Array))
-			return error = `expected function block [${name}]`;
-		list.some(l => {
-			switch (l[0]) {
-			case 'define':
-				v_define(l);
+	const v_block = () => {
+		if (list[pos] !== '(') return false;
+		pos++;
+		// while (pos < list.length)
+		// 	if (error) return false;
+		// 	else if (v_define()
+		return error = `???`, false;
+	};
+
+
+
+	// debugging
+	this.showlist = () => {
+		let out = '', indent = 0, p = 0, last;
+		// get token, highlighting error position
+		const gettok = () => error && p === pos ? `<error>${list[p]}</error>` : list[p];
+		// main loop
+		for (p = 0; p < list.length; p++)
+		switch (list[p]) {
+			case '(':
+				out += (p > 0 ? '\n' : '') + '\t'.repeat(indent) + gettok();
+				last = 'open', indent++;
 				break;
-			case 'set':
-				name = l[1];
-				ex = l[2];
-				v_vname(name) || v_expr(ex);
-				break;
-			case 'if':
-			case 'while':
-				ex = l[1];
-				block = l[2];
-				v_expr(ex) || v_block(block);
+			case ')':
+				out += gettok();
+				last = 'close', indent--;
 				break;
 			default:
-				v_expr(l);
-				// return error = `unexpected in block [${l[0]}]`;
-			}
-			return error;
-		});
-		return error;
+				out += (last === 'close' ? '\n'+'\t'.repeat(indent) : '') 
+					+  (last === 'var' ? ' ' : '') 
+					+  gettok();
+				last = 'var';
+		}
+		// show error, if given
+		if (error) out += `\n\n<error>${error} [${pos}]</error>`
+		return out;
 	};
 };
